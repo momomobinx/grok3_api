b51b608f2a14e0f07777e0fa2307a2e891332de7
GhostXia <33112711+GhostXia@users.noreply.github.com>
2025-03-08 20:44:01 +0800

    Update Test.bat

--- Test.bat
+++ Test.bat
@@ -2,4 +2,4 @@
 chcp 65001 >nul
 
 echo ========== Grok3 API 启动 ==========
-"%~dp0app-test.exe" -token 123456 -cookie sso=eyJhbGciOiJIUzI1NiJ9.eyJzZXNzaW9uX2lkIjoiZmU1NDU4NWUtYzY3YS00OWMwLTllNTAtODYyNGZiZTI3YmIwIn0.u_awejzF9iD5nmYVnXu14DmGbReMTT2HO460c8RCOAc
+"%~dp0app-test.exe" -token 123456 -longtxt
