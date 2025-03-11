d89ab967d3b24eb6e7ad4967508e32b452b25425
GhostXia <33112711+GhostXia@users.noreply.github.com>
2025-03-11 19:28:09 +0800

    修复了启动器在其他电脑上的报错

--- 测试版启动器.bat
+++ 测试版启动器.bat
@@ -1,6 +1,5 @@
 @echo off
 chcp 65001 >nul
 
-echo ========== Grok3 API 启动 ==========
-
-"%~dp0app-test.exe" -token 123456 -longtxt
+echo. ========== Grok3 API 启动 ==========
+"%~dp0app-test.exe" -token 123456 -longtxt 80000
